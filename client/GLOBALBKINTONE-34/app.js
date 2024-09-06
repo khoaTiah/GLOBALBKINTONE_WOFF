82938aaeff548a717d7ebd5a42db573389332738
KhoaNguyen <162974483+khoaTiah@users.noreply.github.com>
2024-09-06 12:00:25 +0700

    GLOBALBKINTONE-34 : update

--- client/GLOBALBKINTONE-34/app.js
+++ client/GLOBALBKINTONE-34/app.js
@@ -286,19 +286,132 @@ const convertUTC = (date) => {
     let utcDate = new Date(Date.UTC(localDate.getFullYear(), localDate.getMonth(), localDate.getDate()));
     return utcDate.toISOString().slice(0, 10);
 }
+const convertTimeEdit = (timeString) => {
+    const date = new Date(timeString);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${month}/${day}/${year}`
+};
 const getRecordByID = async(id) => {
     let appId = 6003;
     axios.get(lambdaUrl + `?id=${appId}&recordId=${id}`)
         .then((res) => {
-            console.log(res);
+            const records = res.data;
+            $("#number-of-floors").val(records.設置階数など_手書き.value);
+            $("#name-of-fire").val(records.名称_点検結果報告書.value);
+            $("#witness").val(records.立会者_点検結果報告書.value);
+            $("#inspection-type").val(records.点検種別_点検結果報告書.value);
+            $("#start-date").val(convertTimeEdit(records.点検年月日_自.value));
+            $("#date-arrival").val(convertTimeEdit(records.点検年月日_至.value));
+            $("#full-name-type").val(records.氏名_消1.value)
+            const row1 = $("#row-1");
+            const row2 = $("#row-2");
+            const row3 = $("#row-3");
+            const row4 = $("#row-4");
+            const row5 = $("#row-5");
+            const row6 = $("#row-6");
+            const row7 = $("#row-7");
+            const row8 = $("#row-8");
+            const row9 = $("#row-9");
+            const row10 = $("#row-10");
+            const row11 = $("#row-11");
+            const row12 = $("#row-12");
+            const row13 = $("#row-13");
+            const row14 = $("#row-14");
+            const row15 = $("#row-15");
+            const row16 = $("#row-16");
+            const row17 = $("#row-17");
+            const row18 = $("#row-18");
+            const row19 = $("#row-19");
+            row1.find("select").val(records.結果A_設場.value);;
+            row1.find("input").first().val(records.不良内容_設場.value);
+            row1.find("input").last().val(records.措置内容_設場.value);
+
+            row2.find("select").val(records.結果A_設間.value);
+            row2.find("input").first().val(records.不良内容_設間.value);
+            row2.find("input").last().val(records.措置内容_設間.value);
+
+            row3.find("select").val(records.結果A_適.value);
+            row3.find("input").first().val(records.不良内容_適.value);
+            row3.find("input").last().val(records.措置内容_適.value);
+
+            row4.find("select").val();
+            row4.find("input").first().val();
+            row4.find("input").last().val();
+
+            row5.find("select").val();
+            row5.find("input").first().val();
+            row5.find("input").last().val();
+
+            row6.find("select").val();
+            row6.find("input").first().val();
+            row6.find("input").last().val();
+
+            row7.find("select").val();
+            row7.find("input").first().val();
+            row7.find("input").last().val();
+
+            row8.find("select").val();
+            row8.find("input").first().val();
+            row8.find("input").last().val();
+
+            row9.find("select").val();
+            row9.find("input").first().val();
+            row9.find("input").last().val();
+
+            row10.find("select").val();
+            row10.find("input").first().val();
+            row10.find("input").last().val();
+
+            row11.find("select").val();
+            row11.find("input").first().val();
+            row11.find("input").last().val();
+
+            row12.find("select").val();
+            row12.find("input").first().val();
+            row12.find("input").last().val();
+
+            row13.find("select").val();
+            row13.find("input").first().val();
+            row13.find("input").last().val();
+
+            row14.find("select").val();
+            row14.find("input").first().val();
+            row14.find("input").last().val();
+
+            row15.find("select").val();
+            row15.find("input").first().val();
+            row15.find("input").last().val();
+
+            row16.find("select").val();
+            row16.find("input").first().val();
+            row16.find("input").last().val();
+
+            row17.find("select").val();
+            row17.find("input").first().val();
+            row17.find("input").last().val();
+
+            row18.find("select").val();
+            row18.find("input").first().val();
+            row18.find("input").last().val();
+
+            row19.find("select").val();
+            row19.find("input").first().val();
+            row19.find("input").last().val();
         })
         .catch((err) => {
             console.error(err);
         });
 }
+const buildPageEdit = async() => {
+    await Promise.all([getRecord_6015(), getRecord_6004()]);
+}
 export function runCreate() {
     buildPageCreate();
 };
 export function runEdit(id) {
+    buildPageEdit();
     getRecordByID(id);
 }
