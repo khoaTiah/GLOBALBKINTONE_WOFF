0b9ed5daf9081c2a279549fb908d1c287b17a438
KhoaNguyen <162974483+khoaTiah@users.noreply.github.com>
2024-10-07 09:09:28 +0700

    38 : add

---
+++ client/GLOBALBKINTONE-37/app.js
@@ -0,0 +1,385 @@
+import { woffId, lambdaUrl } from './params.js';
+window.addEventListener('load', () => {
+    woff.ready.then(() => {
+            woffInit();
+        })
+        // woffInit();
+});
+var dataQR;
+const woffInit = (type = null) => {
+
+    // Initialize WOFF
+    woff.init({ woffId: woffId })
+        .then(() => {
+            if (!woff.isLoggedIn()) {
+                if (checkError()) {
+                    $("#load-main").attr("hidden", true);
+                    return actionSwitch('error');
+                }
+                return woff.login();
+                // woffInit()
+            } else
+            // Success
+            // Get and show user profile
+                getProfile()
+                .then((profile) => {
+                    $("#name-line-works").text(profile.displayName);
+                    if (type == 'create') {
+                        $("#name-line-works").text(profile.displayName);
+                        $("#person-name").val(profile.displayName);
+                    }
+                })
+                .catch((err) => {
+                    window.alert(err);
+                });
+        })
+        .catch((err) => {
+            console.log(err);
+            // Error
+
+        });
+};
+const getProfile = () => {
+    return new Promise((resolve, reject) => {
+        // Get profile
+        woff.getProfile().then((profile) => {
+            // Success
+            console.log(profile)
+            resolve(profile)
+
+        }).catch((err) => {
+            // Error
+            console.error(err)
+            reject(err)
+        });
+    });
+};
+const getQR = async(string) => {
+    // https://cz0899nxjruw.cybozu.com/k/19
+    dataQR = "";
+    $(".qr-code span.message-error").text("");
+    $("#storage-installation").val('');
+    $("#last-time").val('');
+    $(".qr-code input#qr-code").removeClass("input-error");
+    $(".qr-code .coating").attr("hidden", false);
+    let res = await axios.get(lambdaUrl + "?id=19&userCode=k-sinsei&query=" + encodeURIComponent(`設備管理QRコード="${string}"`));
+    const records = res.data;
+    if (records.length <= 0) {
+        $(".qr-code span.message-error").text("設備管理QRコードが存在しません。");
+        $(".qr-code input#qr-code").addClass("input-error");
+        $(".qr-code .coating").attr("hidden", true);
+        return;
+    }
+    // $("#current-mileage").val(formatNumberToComma(records[0].走行距離.value).number);
+    $("#storage-installation").val(records[0].保管設置場所.value);
+    $("#last-time").val(formatNumberToComma(records[0].総走行距離.value).number);
+    dataQR = records[0];
+    $(".qr-code .coating").attr("hidden", true);
+}
+const buildCreatePage = async() => {
+    $("#btn-upload-file").click(function() {
+        document.getElementById('file-input').click();
+    });
+    $('#file-input').change(async function(event) {
+        console.log(event);
+        const file = event.target.files[0];
+        $('.file-info span.message-error').text('');
+        // Kiểm tra định dạng tệp
+        if (!file.type.startsWith('image/')) {
+            $('.file-info span.message-error').text('画像を入力してください。');
+            return;
+        }
+
+        $("#btn-upload-file").addClass("un-active");
+
+        try {
+            const urlS3 = await addFile();
+            const formData = {
+                "path": urlS3.path,
+                "fileName": urlS3.fileName
+            };
+
+            const response = await axios.put(lambdaUrl + '?userCode=k-sinsei', formData, {
+                headers: {}
+            });
+            deletePhoto(formData.fileName);
+            // Sử dụng FileReader để hiển thị ảnh
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                const html = `
+                <div class="file-item row mt-2" id="img-${response.data}">
+                    <div>
+                        <img src="${e.target.result}" alt="" id="${response.data}" class="file-image">
+                        <div>
+                            <span class="file-name">${file.name}</span>
+                            <button type='button' class="delete-file ms-2" onclick="removeFile('img-${response.data}')"><i class="fa fa-times-circle-o" aria-hidden="true"></i></button>
+                        </div>
+                    </div>
+                </div>
+                `;
+                $(".left-file").append(html);
+            };
+
+            reader.readAsDataURL(file);
+            $("#btn-upload-file").removeClass("un-active");
+
+        } catch (error) {
+            console.error(error);
+        }
+    });
+
+    $("#btn-lookup-qr-code").click(function() {
+        $("#qr-code").val("")
+        woff.scanQR()
+            .then((result) => {
+                $("#qr-code").val(result.value);
+                getQR(result.value);
+            })
+            .catch((err) => {
+                alert(err);
+            });
+    });
+    $("button.btn.btn-clear.me-1").click(function() {
+        dataQR = "";
+    });
+    // get profile
+    // document.getElementById('file-input').addEventListener('change', function(event) {
+    //     const file = event.target.files[0]; // Lấy file từ input
+
+    //     if (file) {
+    //         const reader = new FileReader(); // Tạo FileReader để đọc file
+
+    //         reader.onload = function(e) {
+    //             // Khi đọc file xong, gán kết quả vào src của thẻ img
+    //             document.getElementById('demo').src = e.target.result;
+    //         };
+
+    //         reader.readAsDataURL(file); // Đọc file dưới dạng DataURL (base64)
+    //     }
+    // });
+
+    $("#total-mileage").on("change", function() {
+        $(".total-mileage .message-error").text("");
+        $("#total-mileage").removeClass("input-error");
+        $("#mileage").removeClass("input-error");
+        let formatNumber = formatNumberToComma($(this).val());
+        if (!formatNumber.status) {
+            $("#total-mileage").addClass("input-error");
+            return $(".total-mileage .message-error").html("走行距離は数字である必要があります。");
+        }
+        let number = formatNumberRemoveComma($(this).val());
+        // if (!dataQR) return;
+        if (dataQR && (dataQR.総走行距離.value > number)) {
+            $(".total-mileage .message-error").html("入力した走行距離は現在走行距離より小さいです。<br>ご確認ください。");
+            $("#total-mileage").addClass("input-error");
+            $("#mileage").addClass("input-error");
+        }
+
+        $("#total-mileage").val(formatNumber.number);
+        if (dataQR) {
+            $("#mileage").val(formatNumberToComma((number - dataQR.総走行距離.value).toString()).number);
+        }
+
+
+    });
+};
+const clearAll = () => {
+    $("span.message-error").html("");
+}
+const createData = async() => {
+    $("#btn-update-or-add").click(async function() {
+        $("#load-main").attr("hidden", false);
+        $("#qr-code").removeClass("input-error");
+        $("#total-mileage").removeClass("input-error");
+        let qrCode = $("#qr-code").val();
+        let totalMileage = $("#total-mileage").val();
+        let flag = false;
+        if (!qrCode) {
+            $("#qr-code").addClass("input-error");
+            $(".qr-code .message-error").html("設備管理QRコードは必須です。");
+            flag = true;
+        }
+        if (!totalMileage) {
+            $("#total-mileage").addClass("input-error");
+            $(".total-mileage .message-error").html("総走行距離は必須です。");
+            flag = true;
+        }
+        if (flag) {
+            $("#load-main").attr("hidden", true);
+            return;
+        }
+        let err = $(".message-error").text();
+        if (err.length > 0) {
+            $("#load-main").attr("hidden", true);
+            return;
+        }
+        let user_name = await getProfile()
+            .then((profile) => {
+                return profile.displayName;
+            });
+        let vehicle = {
+            "点検_運転_者氏名_0": {
+                "value": user_name,
+            },
+            "設備管理QRコード": {
+                "value": $("#qr-code").val(),
+            },
+            "保管設置場所_変更": {
+                "value": $("#location-change").val(),
+            },
+            "外装チェック乗車前": {
+                "value": $("#boarding").val(),
+            },
+            "総走行距離": {
+                "value": formatNumberRemoveComma($("#total-mileage").val()),
+            },
+        };
+        let arrFile = getFileArray();
+        if (arrFile.length > 0) {
+            vehicle['不具合箇所画像添付'] = {
+                "value": arrFile,
+            }
+        }
+        let bodyManagement = {
+            "総走行距離": {
+                value: formatNumberRemoveComma($("#total-mileage").val())
+            }
+        };
+        if ($("#location-change").val()) {
+            bodyManagement['保管設置場所'] = {
+                "value": $("#location-change").val(),
+            };
+        }
+        let resPut = {
+            id: dataQR.$id.value,
+            body: bodyManagement,
+        };
+        await axios.post(lambdaUrl + "?id=21&userCode=k-sinsei", vehicle)
+            .then(async(res) => {
+                if (res.status == 200) {
+                    const resultUpdate = await updateVehicle(19, resPut);
+                    $("#load-main").attr("hidden", true);
+                    if (!resultUpdate) {
+                        showMessage("データ作成中にエラーが発生しました", 'error');
+                    } else {
+                        let msg = "新しいデータの作成が完了しました！";
+                        if (woff.isInClient()) {
+                            if ($("#boarding").val() == "要修理・交換") {
+                                let msg = "「要修理・交換」が選択されました。";
+                                woff.sendMessage({
+                                        content: msg
+                                    })
+                                    .then(() => {
+                                        woff.closeWindow();
+                                    })
+                                    .catch((err) => {
+                                        console.log('error', err);
+                                    });
+
+                            } else {
+                                woff.sendMessage({
+                                        content: msg
+                                    })
+                                    .then(() => {
+                                        woff.closeWindow();
+                                    })
+                                    .catch((err) => {
+                                        console.log('error', err);
+                                    });
+                            }
+                        }
+                        showMessage(msg, 'success');
+                        setTimeout(function() {
+                            location.reload();
+                        }, 3000);
+                    }
+                }
+            })
+            .catch((err) => {
+                $("#load-main").attr("hidden", true);
+                var data = err.response.data.message
+                $('html, body').animate({
+                    scrollTop: $('.btn-save span').offset().top
+                }, 1000);
+                showMessage("データ作成中にエラーが発生しました", 'error');
+                $(".btn-save span").text(data);
+            });
+    });
+};
+const updateVehicle = async(id, res) => {
+    try {
+        const response = await axios.patch(lambdaUrl + `?id=${id}&&userCode=k-sinsei`, res);
+        if (response.status == 200) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (err) {
+        console.error(err);
+        return false;
+    }
+};
+// S3
+
+
+AWS.config.update({
+    region: bucketRegion,
+    credentials: new AWS.Credentials(accessKeyID, secretAccessKey)
+});
+
+var s3 = new AWS.S3();
+async function addFile() {
+    var folderName = "developer/public";
+    var files = document.getElementById("file-input").files;
+    if (!files.length) {
+        return alert("Please choose a file to upload first.");
+    }
+    var file = files[0];
+    var fileName = Date.now() + "_" + file.name;
+    var albumPhotosKey = folderName + "/";
+    var photoKey = albumPhotosKey + fileName;
+    var upload = new AWS.S3.ManagedUpload({
+        params: {
+            Bucket: s3Name,
+            Key: photoKey,
+            Body: file,
+        },
+    });
+    var promise = upload.promise();
+
+    try {
+        var data = await promise;
+
+        return {
+            "path": `https://${s3Name}.s3.amazonaws.com/${folderName}/${fileName}`,
+            "fileName": fileName
+        }
+    } catch (err) {
+        alert("There was an error uploading your photo: ", err.message);
+    }
+}
+
+function deletePhoto(fileName) {
+    var params = {
+        Bucket: s3Name,
+        Key: "developer/public/" + fileName
+    };
+
+    s3.deleteObject(params, function(err, data) {
+        if (err) {
+            console.error("There was an error deleting the photo: ", err.message);
+        } else {
+            console.log("Photo deleted successfully");
+        }
+    });
+}
+
+export function runCreate() {
+    woffInit('create');
+    buildCreatePage();
+    clearAll();
+    createData();
+};
+export function runEdit(id) {
+    clearAll();
+}
---
+++ client/GLOBALBKINTONE-37/script.js
@@ -0,0 +1,171 @@
+const PATH_MENU = './menu/index.html';
+const PATH_ADD = './add/index.html';
+const PATH_EDIT = './edit/index.html';
+const PATH_LOGIN = './login/index.html';
+const PATH_ERROR = './error/index.html';
+const JS_HELPER = './app.js';
+const JS_LIST = './edit/app.js'
+const CSS_MAIN = './style.css';
+const CSS_LIST = './edit/style.css';
+const CSS_PAGINATION = './edit/style_pagination_js.css';
+
+window.addEventListener('load', async() => {
+    actionSwitch('add');
+});
+
+function actionSwitch(action, id = "") {
+    const paths = {
+        add: PATH_ADD,
+        edit: PATH_ADD,
+        menu: PATH_MENU,
+        list: PATH_EDIT,
+        login: PATH_LOGIN,
+        error: PATH_ERROR,
+    };
+
+    const path = paths[action] || PATH_MENU;
+
+    $("#main").load(path, function(response, status, xhr) {
+        if (status == "success") {
+            loadScript(JS_HELPER, action, id);
+            loadCss([CSS_MAIN]);
+        }
+        // if (action === 'add') {
+        //     loadCreate();
+        // }
+        if (action === 'edit') {
+            // loadCreate();
+
+            // backList();
+        }
+        if (action == "list") {
+            loadScript(JS_LIST, action, id);
+            loadCss([CSS_LIST, CSS_PAGINATION]);
+            loadCss([CSS_LIST, CSS_PAGINATION]);
+        }
+    });
+}
+
+async function loadScript(scriptPath, action, id = "") {
+
+    const module = await
+    import (scriptPath);
+    if (action == "list") {
+        if (module.run) {
+            module.run();
+        }
+    }
+    if (action == "edit") {
+        if (module.runEdit) {
+            module.runEdit(id);
+        }
+    }
+    if (action == "add") {
+        if (module.runCreate) {
+            module.runCreate();
+        }
+    }
+}
+
+function loadCss(cssArray) {
+    const existingLinks = document.querySelectorAll('link[id^="dynamicCss"]');
+    existingLinks.forEach(link => link.parentNode.removeChild(link));
+    cssArray.forEach((cssPath, index) => {
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = cssPath + "?t=" + new Date().getTime(); // caching
+        link.id = `dynamicCss-${index}`;
+        document.head.appendChild(link);
+    });
+}
+// page list
+function switchDisplayData(key) {
+    switch (key) {
+        case "table":
+            $("#btn-detail").removeClass("active");
+            $("#btn-list").addClass("active");
+            $("#ls").removeClass("display-none");
+            $("#ls-mobile").addClass("display-none");
+            break;
+        case "card":
+            $("#btn-detail").addClass("active");
+            $("#btn-list").removeClass("active");
+            $("#ls").addClass("display-none");
+            $("#ls-mobile").removeClass("display-none");
+            break;
+        default:
+            alert('error');
+    }
+}
+const showModelEdit = (id) => {
+    actionSwitch('edit', id);
+}
+var showModalDelete = (id) => {
+    $("#modal-notification").show();
+    $(".id-remove-app").val(id);
+    $("#remove-id").text(id);
+}
+const showMessage = (mess, type) => {
+    if (type == "error") {
+        $("#toast").removeClass("success warning").addClass("error");
+        $("#toast .toast-header strong").text("エラー");
+    } else if (type == "success") {
+        $("#toast").removeClass("error warning").addClass("success");
+        $("#toast .toast-header strong").text("成功");
+    } else if (type == "warning") {
+        $("#toast").removeClass("error success").addClass("warning");
+        $("#toast .toast-header strong").text("警告");
+    } else {
+        return;
+    }
+    $("#toast .toast-body span").text(mess);
+    $("#toast").removeClass('display-none');
+    $("#toast .toast-body span").text(mess);
+    $('#toast').toast('show');
+}
+const clearLookupQR = () => {
+    $("#qr-code").val("");
+    $(".qr-code span.message-error").text("");
+    $(".qr-code input#qr-code").removeClass("input-error");
+    $("#storage-installation").val('');
+    $("#last-time").val('');
+}
+const formatNumberToComma = (number) => {
+    let value = number.replace(/,/g, '');
+    if (!isNaN(value)) {
+        return {
+            status: true,
+            number: value.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+        }
+    }
+    return { status: false, number: 0 };
+}
+const formatNumberRemoveComma = (number) => {
+    return Number(number.replace(/,/g, ''));
+}
+const convertTime = (timeString) => {
+    const date = new Date(timeString);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+};
+const checkError = () => {
+    const currentUrl = window.location.href;
+    const urlParams = new URL(currentUrl);
+    const searchParams = new URLSearchParams(urlParams.search);
+    const errorDescription = searchParams.get('error_description');
+    return errorDescription;
+}
+const removeFile = (idHTML) => {
+    $(`#${idHTML}`).remove();
+}
+const getFileArray = () => {
+    const images = document.querySelectorAll('.left-file img');
+    const imageIds = Array.from(images).map(img => ({ fileKey: img.id }));
+    return imageIds;
+}
